d91a19eab730d4be778d391fe4b05312f18440a7
LAPTOP-K80UQUAL\naper <naperloff@hotmail.com>
2018-04-02 22:25:13 -0500

    inventory scripto

---
+++ Assets/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    #region Singleton
+    public static Inventory instance;
+    void Awake()
+    {
+       if (instance!=null)
+        {
+            Debug.LogWarning("There is already an instance of the Inventory!");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public List<GameObject> items = new List<GameObject>();
+
+    void add(GameObject g)
+    {
+        items.Add(g);
+    }
+
+    void remove(GameObject g)
+    {
+        items.Remove(g);
+    }
+}
